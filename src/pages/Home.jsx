4d97f180411068a919339d2ce47a55f4b1eace83
VickkiMars <victorumoreng@gmail.com>
2026-04-07 06:33:16 +0100

    chore: remove unused clone artifacts and components from tracking and update gitignore

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-// pages/Home.jsx
-export default function Home() {
-  return (
-    <div className="p-10 text-center">
-      <h1 className="text-3xl font-bold">Home</h1>
-      <p className="mt-4 text-gray-600">No Resources yet</p>
-    </div>
-  );
-}
